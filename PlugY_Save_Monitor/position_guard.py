bccdc38be903a20ac2b22b2c86bf0b39af5c6243
Jyjyc <jyc.mindenhol@gmail.com>
2023-08-05 00:17:44 +0200

    Plugy addons

--- PlugY_Save_Monitor/position_guard.py
+++ PlugY_Save_Monitor/position_guard.py
@@ -1,6 +1,5 @@
 import os
 import time
-import platform
 import errno
 
 from datetime import datetime
@@ -10,18 +9,12 @@ if __name__ == '__main__':
 
     ####### USER SETTING #######
     char_name = "jyc"
-    char2_name = "fsdfa"
-#    if platform.system() == 'Linux':
-#        save_dir = '/mnt/c/Users/victo/Dropbox (Personal)/D2Save'
-#    else if platform.system() == 'Windows'
     save_dir = 'C:\Programs\Diablo II\save'
-
-    # Specify char name and position on select screen
-    #chars = [(char1_name, 1), (char2_name, 2)]
-    ####### END USER SETTING #######
 
     #save_suffixes = ['.d2s', '.d2x', '.ma0', '.ma1', '.ma2', '.ma3', '.key']
     save_suffixes = ['.d2s', '.d2x', '.ma0', '.key']
+
+    ####### END USER SETTING #######
 
     start_time = time.time()
     while True:
@@ -32,16 +25,5 @@ if __name__ == '__main__':
             if mod_time != target_time:
                 print('modified ', file_path)
                 os.utime(file_path, (target_time, target_time))
-
-#    start_time = time.time()
-#    while True:
-#        for char, position in chars:
-#            target_time = start_time - position * 1000
-#            for suffix in save_suffixes:
-#                file_path = os.path.join(save_dir, char + suffix)
-#                mod_time = os.path.getmtime(file_path) 
-#                if mod_time != target_time:
-#                    print('modified ', file_path)
-#                    os.utime(file_path, (target_time, target_time))
         time.sleep(1.5)
 
---
+++ position_guard.cmd
@@ -0,0 +1,4 @@
+cd %~dp0PlugY_Save_Monitor\
+dir
+python -m position_guard
+pause
